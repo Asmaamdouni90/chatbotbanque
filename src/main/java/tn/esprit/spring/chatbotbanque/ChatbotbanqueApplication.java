d7148cb9981fa4f7dd82baf27ef0232f2341521f
unknown <amdouni.asma@esprit.tn>
2021-07-30 09:19:06 +0200

    first commit

---
+++ src/main/java/tn/esprit/spring/chatbotbanque/ChatbotbanqueApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.spring.chatbotbanque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotbanqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotbanqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/esprit/spring/chatbotbanque/ChatbotbanqueApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.spring.chatbotbanque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotbanqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
